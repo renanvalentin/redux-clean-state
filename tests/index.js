ff2d3e21ed4365aed55ac338dcd2f73dfd07806e
Renan Ferreira <renan.lifestyle@hotmail.com>
2016-06-12 20:16:18 -0400

    setup

---
+++ tests/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import sd from 'skin-deep';
+
+import equalJSX from 'chai-equal-jsx';
+chai.use(equalJSX);
+
+import ComposeClearAppState from 'components/composes/ComposeClearAppState';
+
+describe('(Components/Composes) ComposeClearAppState', () => {
+  describe('Rendering', () => {
+    it('Should render the component passed to ComposeClearAppState', () => {
+      const Component = ComposeClearAppState(
+        <div></div>
+      );
+
+      const tree = sd.shallowRender(<Component />);
+      const vdom = tree.getRenderOutput();
+
+      expect(vdom.type.type).to.be.equal('div');
+    });
+  });
+
+  describe('Dispatch action', () => {
+    it('it should dispatch and action on componentWillUnmount', () => {
+      const componentToBeRendered = (
+        <div />
+      );
+
+      const action = 'RESET_STATE';
+
+      const Component = ComposeClearAppState(componentToBeRendered, action);
+
+      const tree = sd.shallowRender(<Component />);
+      const mountedInstance = tree.getMountedInstance();
+
+      const dispatchSpy = sinon.spy();
+
+      mountedInstance.context = {
+        store: {
+          dispatch: dispatchSpy
+        }
+      };
+
+      mountedInstance.componentWillUnmount();
+      expect(dispatchSpy).to.have.been.calledWith({type: action});
+    });
+  });
+});
+
